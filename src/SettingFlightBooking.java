b2156ab01d74efd2f9bcc49c161425fe937a1a1d
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-19 22:11:08 +0200

    Add airline setting method

--- src/SettingFlightBooking.java
+++ src/SettingFlightBooking.java
@@ -4,6 +4,7 @@ public class SettingFlightBooking {
     public static void main(String[] args) {
 
         Scanner obScanner                    = new Scanner(System.in);
+        SwitchAirline settingAirline         = new SwitchAirline();
         FlightBooking settingBooking         = new FlightBooking();
         SwitchDestination settingDestination = new SwitchDestination();
 
@@ -13,11 +14,18 @@ public class SettingFlightBooking {
 
         System.out.println("Choose your destination");
         System.out.print("Paris, Roma, London, Barcelona: ");
-        String destination = obScanner.next().toLowerCase();
-        settingDestination.controlDestiantion(destination);
-        destination = settingDestination.finalDestinationName;
-        settingBooking.setDestination(destination);
-        System.out.println(settingBooking.getDestination());
+        String destinationName = obScanner.next().toLowerCase();
+        settingDestination.controlDestiantion(destinationName);
+        destinationName = settingDestination.finalDestinationName;
+        settingBooking.setDestination(destinationName);
+
+        System.out.println("Choose your airline");
+        System.out.print("WizzAir, RyanAir, TurkishAirline, HiSky: ");
+        String airlineName = obScanner.next().toLowerCase();
+        settingAirline.controlAirline(airlineName);
+        airlineName = settingAirline.finalAirlineName;
+
+
 
 
 
---
+++ src/SwitchAirline.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SwitchAirline {
+    String finalAirlineName;
+
+    void controlAirline(String airlineName) {
+        Scanner obScanner = new Scanner(System.in);
+        boolean valid = false;
+
+        do {
+            switch (airlineName) {
+                case "wizzair":
+                    System.out.println("You chose the airline WizzAir!");
+                    finalAirlineName = "WizzAir";
+                    valid = true;
+                    break;
+
+                case "ryanair":
+                    System.out.println("You chose the airline RyanAir!");
+                    finalAirlineName = "RyanAir";
+                    valid = true;
+                    break;
+
+                case "turkishairlines":
+                    System.out.println("You chose the airline TurkishAirlines!");
+                    finalAirlineName = "TurkishAirlines";
+                    valid = true;
+                    break;
+
+                case "hisky":
+                    System.out.println("You chose the airline HiSky!");
+                    finalAirlineName = "HiSky";
+                    valid = true;
+                    break;
+
+                default:
+                    System.out.println("Unknown airline company! Choose again!");
+                    System.out.print("WizzAir, RyanAir, TurkishAirline, HiSky: ");
+                    airlineName = obScanner.next().toLowerCase();
+                    valid = false;
+            }
+        } while (!valid);
+
+    }
+}
