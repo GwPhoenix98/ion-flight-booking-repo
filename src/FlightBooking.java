d54c2bf1cbd8efb47e4b721213d43d4462d70f5e
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-19 19:12:11 +0200

    Initial commit

---
+++ src/FlightBooking.java
@@ -0,0 +1,48 @@
+public class FlightBooking {
+
+    private String  passengerName,
+                    destination,
+                    airline,
+                    seatClass;
+    private boolean hasLuggage;
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getSeatClass() {
+        return seatClass;
+    }
+
+    public void setSeatClass(String seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public boolean isHasLuggage() {
+        return hasLuggage;
+    }
+
+    public void setHasLuggage(boolean hasLuggage) {
+        this.hasLuggage = hasLuggage;
+    }
+}
